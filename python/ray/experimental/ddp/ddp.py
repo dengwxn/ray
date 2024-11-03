9316c772aaeef437c24383424ebc04462244b0b5
Yuhan Ruan <andyubryh@gmail.com>
2024-11-02 21:57:23 -0700

    chore: Refactor
    Signed-off-by: Yuhan Ruan <andyubryh@gmail.com>

--- python/ray/experimental/ddp/ddp.py
+++ python/ray/experimental/ddp/ddp.py
@@ -51,62 +51,6 @@ SEED = 42
 set_seed(SEED)
 
 
-class DDPModel(ABC):
-    """A layered model that uses distributed data parallel."""
-
-    def __init__(self, num_layers: int):
-        self._num_layers = num_layers
-        self._device = torch_utils.get_devices()[0]
-
-    @property
-    def num_layers(self) -> int:
-        return self._num_layers
-
-    def train(self, X: torch.Tensor, Y: torch.Tensor) -> torch.Tensor:
-        input = self.start_train(X)
-        for i in range(self.num_layers):
-            input = self.forward(i, input)
-        pred = input
-        grad = self.loss(pred, Y)
-        updates = []
-        for i in reversed(range(self.num_layers)):
-            grad, grad_update = self.backward(i, grad)
-            updates.append(self.update(i, grad_update))
-        return self.finish_train(*updates)
-
-    @abstractmethod
-    def start_train(self, X: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
-
-    @abstractmethod
-    def forward(self, layer_idx: int, input: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
-
-    @abstractmethod
-    def loss(self, pred: torch.Tensor, Y: torch.Tensor) -> Tuple[torch.Tensor, None]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def backward(
-        self, layer_idx: int, grad: Tuple[torch.Tensor, Optional[torch.Tensor]]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def update(self, layer_idx: int, grad: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
-
-    @abstractmethod
-    def finish_train(self, *updates: torch.Tensor) -> List[torch.Tensor]:
-        raise NotImplementedError
-
-    def get_grad_to_reduce(
-        self, grad: Tuple[torch.Tensor, torch.Tensor]
-    ) -> torch.Tensor:
-        _, reduce_grad = grad
-        return reduce_grad
-
-
 class LayeredModel(torch.nn.Module):
     """A model that is a chain of (linear, relu) layers."""
 
@@ -116,6 +60,7 @@ class LayeredModel(torch.nn.Module):
         num_layers: int,
         device: torch.device,
         dtype: torch.dtype,
+        lr: float,
     ):
         torch.manual_seed(SEED)
         torch.cuda.manual_seed_all(SEED)
@@ -133,7 +78,7 @@ class LayeredModel(torch.nn.Module):
         self.layers: nn.ModuleList = nn.ModuleList(self.layers)
         self.inputs: List[torch.Tensor] = []
         self.activations: List[torch.Tensor] = []
-        self.lr: float = CONFIG.lr
+        self.lr: float = lr
         self.criterion = nn.MSELoss()
         self.optimizers: List[optim.SGD] = [
             optim.SGD(self.layers[2 * i].parameters(), lr=self.lr)
@@ -166,43 +111,39 @@ class LayeredModel(torch.nn.Module):
         z.backward(gradient=grad, retain_graph=True, inputs=[W, x])
         return x.grad, W.grad
 
-    def update_layer(
-        self, grad: torch.Tensor, layer_idx: int, return_weight: bool = True
-    ) -> torch.Tensor:
+    def update_layer(self, grad: torch.Tensor, layer_idx: int) -> torch.Tensor:
         layer: torch.nn.Linear = self.layers[2 * layer_idx]
         [param for param in layer.parameters()][0].grad = grad
         optimizer = self.optimizers[layer_idx]
         optimizer.step()
-        return layer.weight if return_weight else None
+        return layer.weight
 
 
 @ray.remote
-class RayDDPWorker(DDPModel):
+class RayDDPWorker:
     """An actor class wrapper around the pytorch model."""
 
-    def __init__(self, num_layers: int, layer_size: int, world_size: int):
-        super().__init__(num_layers)
+    def __init__(self, num_layers: int, layer_size: int, world_size: int, lr: float):
+        self.num_layers = num_layers
+        self.device = torch_utils.get_devices()[0]
 
         self.model: LayeredModel = LayeredModel(
-            layer_size, num_layers, self._device, torch.float32
+            layer_size, num_layers, self.device, torch.float32, lr
         )
         self.world_size: int = world_size
 
-    def start_train(self, x: torch.Tensor) -> torch.Tensor:
+    def start_train(self) -> None:
         self.model.zero_grad()
         self.model.inputs = []
         self.model.activations = []
-        self.model.loss = None
-        self.model.pred = None
-        return x.to(self._device)
 
-    def forward(self, layer_idx: int, input: torch.Tensor) -> torch.Tensor:
-        input = input.to(self._device)
-        return self.model.forward_layer(input, layer_idx)
-
-    def loss(self, pred: torch.Tensor, y: torch.Tensor) -> Tuple[torch.Tensor, None]:
-        y = y.to(self._device)
-        pred = pred.to(self._device)
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        self.start_train()
+        x = x.to(self.device)
+        y = y.to(self.device)
+        for i in range(self.num_layers):
+            x = self.model.forward_layer(x, i)
+        pred = x
         loss: torch.Tensor = self.model.criterion(pred, y)
         loss.backward(retain_graph=True, inputs=[pred])
         return pred.grad, None
@@ -211,18 +152,28 @@ class RayDDPWorker(DDPModel):
         self, layer_idx: int, grad: Tuple[torch.Tensor, Optional[torch.Tensor]]
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         bp_grad, _ = grad
-        bp_grad = bp_grad.to(self._device)
+        bp_grad = bp_grad.to(self.device)
         return self.model.backward_layer(bp_grad, layer_idx)
 
-    def update(
-        self, layer_idx: int, grad: torch.Tensor, return_weight: bool = True
-    ) -> torch.Tensor:
-        grad = grad.to(self._device)
+    def update(self, layer_idx: int, grad: torch.Tensor) -> torch.Tensor:
+        grad = grad.to(self.device)
         grad /= self.world_size
         return self.model.update_layer(grad, layer_idx)
 
     def finish_train(self, *updates: torch.Tensor) -> List[torch.Tensor]:
+        """
+        Gathers all weights across different layers.
+
+        Args:
+            updates: A tuple of any number of updated weights.
+        """
         return updates
+
+    def get_grad_to_reduce(
+        self, grad: Tuple[torch.Tensor, torch.Tensor]
+    ) -> torch.Tensor:
+        _, reduce_grad = grad
+        return reduce_grad
 
 
 def generate_input_output(config: Config) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -244,82 +195,24 @@ def generate_input_output(config: Config) -> Tuple[torch.Tensor, torch.Tensor]:
 
 def print_elapses(elapses, desc):
     """Print individual elapses and their average."""
+
+    def s_to_us(seconds):
+        """Converts seconds to microseconds (rounded)."""
+        return round(seconds * 1e6)
+
     for i, elapse in enumerate(elapses):
-        print(f"{desc} #{i} elapse={elapse}")
+        print(f"{desc} #{i} elapse={s_to_us(elapse)} us")
     total = sum(elapses)
     avg = total / len(elapses)
-    print(f"{desc} avg: {avg}")
+    print(f"{desc} avg: {s_to_us(avg)} us")
     total -= elapses[0]
     avg = total / (len(elapses) - 1)
-    print(f"{desc} avg w/o 1st iter: {avg}")
+    print(f"{desc} avg w/o 1st iter: {s_to_us(avg)} us")
 
 
-def measure_adag_ddp_perf(model: Type[DDPModel]):
-    """
-    Measure the performance of Ray DDP with aDAG and allreduce.
-    For performance, only serialize the tensors to be allreduced (WIP).
-    """
-    actor_cls = model.options(num_gpus=1)
-    num_layers, layer_size = CONFIG.num_layers, CONFIG.layer_size
-    num_actors = CONFIG.num_actors
-    actors = [
-        actor_cls.remote(num_layers, layer_size, num_actors) for _ in range(num_actors)
-    ]
-
-    with InputNode() as inp:
-        losses = []
-        for i, actor in enumerate(actors):
-            x = inp[i]
-            y = inp[num_actors + i]
-            start = actor.start_train.bind(x)
-            forwards = [start]
-            for j in range(num_layers):
-                forwards.append(actor.forward.bind(j, forwards[-1]))
-            loss = actor.loss.bind(forwards[-1], y)
-            losses.append(loss)
-        output = []
-        grads = losses
-        for j in reversed(range(num_layers)):
-            for i, actor in enumerate(actors):
-                grads[i] = actor.backward.bind(j, grads[i])
-            reduced_grads = allreduce.bind(
-                [
-                    actor.get_grad_to_reduce.bind(grads[i])
-                    for i, actor in enumerate(actors)
-                ]
-            )
-            updates = [
-                actor.update.bind(j, reduced_grad, False)
-                for actor, reduced_grad in zip(actors, reduced_grads)
-            ]
-            for update in updates:
-                output.append(update)
-        dag = MultiOutputNode(output)
-
-    compiled_dag = dag.experimental_compile()
-    it = CONFIG.it
-    results = []
-    elapses = []
-    for i in range(it):
-        x, y = generate_input_output(CONFIG)
-        xs = torch.tensor_split(x, num_actors)
-        ys = torch.tensor_split(y, num_actors)
-        start = time.perf_counter()
-        ref = compiled_dag.execute(*xs, *ys)
-        result = ray.get(ref)
-        end = time.perf_counter()
-        results.append(result)
-        elapse = end - start
-        elapses.append(elapse)
-
-    print_elapses(elapses, "adag ddp")
-    compiled_dag.teardown()
-
-    for actor in actors:
-        ray.kill(actor)
-
-
-def get_adag_ddp_weights_per_device(weights: List[List[Tuple[torch.Tensor]]]):
+def get_adag_ddp_weights_per_device(
+    weights: List[List[Tuple[torch.Tensor]]], num_actors: int
+):
     """
     Check that the model is consistent across all ranks after each iteration
     of training. Deduplicate the weights after the checks.
@@ -331,7 +224,7 @@ def get_adag_ddp_weights_per_device(weights: List[List[Tuple[torch.Tensor]]]):
     weights_per_device = []
     for single_iter_weights in weights:
         per_device_len = len(single_iter_weights[0])
-        for i in range(1, CONFIG.num_actors):
+        for i in range(1, num_actors):
             for j in range(per_device_len):
                 assert torch.equal(single_iter_weights[0][j], single_iter_weights[i][j])
         weights_per_device.append(single_iter_weights[0])
@@ -374,26 +267,29 @@ def compare_weights(W1, W2, desc):
             ), f"{desc} max diff: {torch.max(torch.abs(t1 - t2).flatten())}"
 
 
-def check_correctness(adag_ddp_weights) -> None:
+def check_correctness(adag_ddp_weights, config: Config) -> None:
     """Compare aDAG DDP weights with pytorch and pytorch DDP weights."""
-    adag_ddp_weights = get_adag_ddp_weights_per_device(adag_ddp_weights)
-    torch_weights = run_torch()
+    adag_ddp_weights = get_adag_ddp_weights_per_device(
+        adag_ddp_weights, config.num_actors
+    )
+    torch_weights = run_torch(config)
     compare_weights(adag_ddp_weights, torch_weights, "adag ddp vs torch")
     adag_ddp_weights = detach_all(adag_ddp_weights)
     torch_weights = detach_all(torch_weights)
-    run_torch_ddp(torch_weights, adag_ddp_weights)
+    run_torch_ddp(torch_weights, config)
 
 
-def run_adag_ddp(model: Type[DDPModel]):
+def run_adag_ddp(config: Config):
     """
-    Check the correctness of DDP using aDAG and allreduce in Ray.
-    Return the updated weights after each iteration.
+    Run DDP using aDAG and allreduce in Ray. Return the updated weights after each
+    iteration.
     """
-    actor_cls = model.options(num_gpus=1)
-    num_layers, layer_size = CONFIG.num_layers, CONFIG.layer_size
-    num_actors = CONFIG.num_actors
+    actor_cls = RayDDPWorker.options(num_gpus=1)
+    num_layers, layer_size = config.num_layers, config.layer_size
+    num_actors = config.num_actors
     actors = [
-        actor_cls.remote(num_layers, layer_size, num_actors) for _ in range(num_actors)
+        actor_cls.remote(num_layers, layer_size, num_actors, config.lr)
+        for _ in range(num_actors)
     ]
 
     with InputNode() as inp:
@@ -401,12 +297,7 @@ def run_adag_ddp(model: Type[DDPModel]):
         for i, actor in enumerate(actors):
             x = inp[i]
             y = inp[num_actors + i]
-            start = actor.start_train.bind(x)
-            forwards = [start]
-            for j in range(num_layers):
-                forwards.append(actor.forward.bind(j, forwards[-1]))
-            loss = actor.loss.bind(forwards[-1], y)
-            losses.append(loss)
+            losses.append(actor.forward.bind(x, y))
         output = []
         grads = losses
         for j in reversed(range(num_layers)):
@@ -432,16 +323,22 @@ def run_adag_ddp(model: Type[DDPModel]):
         dag = MultiOutputNode(ends)
 
     compiled_dag = dag.experimental_compile()
-    it = CONFIG.it
+    it = config.it
     adag_ddp_weights = []
+    elapses = []
     for i in range(it):
-        x, y = generate_input_output(CONFIG)
+        x, y = generate_input_output(config)
         xs = torch.tensor_split(x, num_actors)
         ys = torch.tensor_split(y, num_actors)
+        x = time.perf_counter()
         ref = compiled_dag.execute(*xs, *ys)
         cur_iter_weights = ray.get(ref)
+        end = time.perf_counter()
         adag_ddp_weights.append(cur_iter_weights)
+        elapse = end - x
+        elapses.append(elapse)
 
+    print_elapses(elapses, "adag ddp")
     compiled_dag.teardown()
 
     for actor in actors:
@@ -450,17 +347,19 @@ def run_adag_ddp(model: Type[DDPModel]):
     return adag_ddp_weights
 
 
-def run_torch():
-    """Run basic pytorch without DDP and return the weights."""
+def run_torch(config):
+    """Run pytorch without DDP and return the weights."""
     device = "cuda:0"
-    model = LayeredModel(CONFIG.layer_size, CONFIG.num_layers, device, CONFIG.dtype)
+    model = LayeredModel(
+        config.layer_size, config.num_layers, device, config.dtype, config.lr
+    )
     criterion = model.criterion
     optimizer = optim.SGD(model.parameters(), lr=model.lr)
 
     torch_weights = []
     elapses = []
-    for i in range(CONFIG.it):
-        x, y = generate_input_output(CONFIG)
+    for i in range(config.it):
+        x, y = generate_input_output(config)
         x = x.to(device)
         y = y.to(device)
         start = time.perf_counter()
@@ -485,22 +384,22 @@ def run_torch():
     return torch_weights
 
 
-def run_torch_ddp(torch_weights, adag_ddp_weights):
+def run_torch_ddp(torch_weights, config: Config):
     """Run DDP with `torch.distributed`. Check and compare the weights."""
     n_gpus = torch.cuda.device_count()
     assert (
-        n_gpus >= CONFIG.num_actors
-    ), f"Requires at least {CONFIG.num_actors} GPUs to run, but got {n_gpus}"
+        n_gpus >= config.num_actors
+    ), f"Requires at least {config.num_actors} GPUs to run, but got {n_gpus}"
     world_size = n_gpus
     mp.spawn(
         torch_dist_run,
-        args=(world_size, torch_weights, adag_ddp_weights),
+        args=(world_size, torch_weights, config),
         nprocs=world_size,
         join=True,
     )
 
 
-def torch_dist_run(rank, world_size, torch_weights, adag_ddp_weights):
+def torch_dist_run(rank, world_size, torch_weights, config: Config):
     """Run DDP with `torch.distributed`."""
     print(f"Running torch DDP on rank {rank}.")
     os.environ["MASTER_ADDR"] = "localhost"
@@ -511,19 +410,19 @@ def torch_dist_run(rank, world_size, torch_weights, adag_ddp_weights):
 
     # create model and move it to GPU with id rank
     model = LayeredModel(
-        CONFIG.layer_size, CONFIG.num_layers, f"cuda:{rank}", CONFIG.dtype
+        config.layer_size, config.num_layers, f"cuda:{rank}", config.dtype, config.lr
     ).to(rank)
     ddp_model = DDP(model, device_ids=[rank])
 
     loss_fn = model.criterion
     optimizer = optim.SGD(ddp_model.parameters(), lr=model.lr)
 
-    num_actors = CONFIG.num_actors
+    num_actors = config.num_actors
 
     torch_ddp_weights = []
     elapses = []
-    for i in range(CONFIG.it):
-        x, y = generate_input_output(CONFIG)
+    for i in range(config.it):
+        x, y = generate_input_output(config)
         xs = torch.tensor_split(x, num_actors)
         ys = torch.tensor_split(y, num_actors)
         x = xs[rank]
@@ -552,21 +451,20 @@ def torch_dist_run(rank, world_size, torch_weights, adag_ddp_weights):
 
     dist.destroy_process_group()
     compare_weights(torch_weights, torch_ddp_weights, "torch vs torch ddp")
-    compare_weights(adag_ddp_weights, torch_ddp_weights, "adag ddp vs torch ddp")
 
 
 def main() -> None:
+    config = CONFIG
     ray.init()
-    if sum(node["Resources"].get("GPU", 0) for node in ray.nodes()) < CONFIG.num_actors:
-        print(f"Needs at least {CONFIG.num_actors} GPUs")
+    if sum(node["Resources"].get("GPU", 0) for node in ray.nodes()) < config.num_actors:
+        print(f"Needs at least {config.num_actors} GPUs")
         return
 
-    adag_ddp_weights = run_adag_ddp(RayDDPWorker)
-    measure_adag_ddp_perf(RayDDPWorker)
+    adag_ddp_weights = run_adag_ddp(config)
 
     ray.shutdown()
 
-    check_correctness(adag_ddp_weights)
+    check_correctness(adag_ddp_weights, config)
 
 
 if __name__ == "__main__":
