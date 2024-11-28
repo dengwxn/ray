7fbb59d47865d561b12e6cfe619b7f18afe792ff
Yuhan Ruan <andyubryh@gmail.com>
2024-11-27 21:39:10 -0800

    chore: Reorg files
    Signed-off-by: Yuhan Ruan <andyubryh@gmail.com>

--- python/ray/experimental/ddp/allreduce-debug-profile.py
+++
@@ -1,7 +0,0 @@
-import pstats
-
-# Load the profiling data
-stats = pstats.Stats("allreduce.prof")
-
-# Sort by cumulative time and print the top 10 results
-stats.sort_stats("cumulative").print_stats()
