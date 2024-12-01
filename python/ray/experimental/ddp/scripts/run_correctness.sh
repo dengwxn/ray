b38790552743b094caf7c009b2d5299a9768599a
Weixin Deng <weixin@cs.washington.edu>
2024-11-30 18:16:16 -0800

    chore: Reorg core
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/scripts/run_correctness.sh
+++ python/ray/experimental/ddp/scripts/run_correctness.sh
@@ -11,13 +11,13 @@ num_actors=2
 mkdir -p $result
 
 RAY_DEDUP_LOGS=0 \
-    python3 src/ddp.py \
+    python -m ray.experimental.ddp.src.ddp \
+    --dtype $dtype \
+    --num-actors $num_actors \
     --num-layers $num_layers \
     --layer-size $layer_size \
-    --dtype $dtype \
     --num-iters $num_iters \
     --learning-rate $learning_rate \
-    --num-actors $num_actors \
-    --check-correctness \
     --output-file $result/latency.csv \
+    --check-correctness \
     >$result/run.log 2>&1
---
+++ python/ray/experimental/ddp/src/core/common.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+
+import torch
+
+from .config import Config
+
+
+def secs_to_micros(secs: float) -> int:
+    """
+    Converts seconds to microseconds (rounded).
+    """
+    return round(secs * 1e6)
+
+
+def generate_input_output(config: Config) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Generate input `x` and output `y` for training.
+
+    Args:
+        config: Model and training configurations.
+
+    Returns:
+        Input `x` and ground truth `y`.
+    """
+    layer_size = config.layer_size
+    num_actors = config.num_actors
+    dtype = config.dtype
+
+    shape = (num_actors * layer_size, layer_size)
+    numel = shape[0] * shape[1]
+
+    x = torch.arange(numel, dtype=dtype, requires_grad=True) / numel
+    x = x.reshape(shape)
+    y = torch.arange(numel, dtype=dtype) / numel
+    y = y.reshape(shape)
+
+    return x, y
