37b3a4eefb35114bf776f6a5a75a14b4bcf805d5
Weixin Deng <weixin@cs.washington.edu>
2024-12-30 16:34:50 -0800

    pivot: Train cot version
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/scripts/barbell/element/unit.sh
+++ python/ray/experimental/ddp/scripts/barbell/element/unit.sh
@@ -12,7 +12,11 @@ output_path=results/barbell/element/unit
 mkdir -p $output_path
 rm -f $output_path/*.csv
 
-modes=(sequential checkpoint)
+modes=(
+	sequential
+	# checkpoint
+	cot
+)
 for mode in ${modes[@]}; do
 	output_file=$output_path/unit_${timestamp}_${mode}.log
 	model_file=$output_path/unit_${timestamp}_${mode}_model.log
@@ -23,17 +27,24 @@ for mode in ${modes[@]}; do
 		>$output_file 2>&1
 done
 
+code $output_path/unit_${timestamp}_${modes[0]}.log
+code $output_path/unit_${timestamp}_${modes[1]}.log
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 if ! diff \
 	"$output_path/unit_${timestamp}_${modes[0]}_model.log" \
 	"$output_path/unit_${timestamp}_${modes[1]}_model.log"; then
 	case $? in
-	1) echo "WA" ;;
-	*) echo "ER" ;;
+	1) echo -e "${YELLOW}WA${NC}" ;;
+	*) echo -e "${RED}ER${NC}" ;;
 	esac
+	code $output_path/unit_${timestamp}_${modes[0]}_model.log
+	code $output_path/unit_${timestamp}_${modes[1]}_model.log
 	exit 1
 else
-	echo "AC"
+	echo -e "${GREEN}AC${NC}"
 fi
-
-code $output_path/unit_${timestamp}_${modes[0]}.log
-code $output_path/unit_${timestamp}_${modes[1]}.log
--- python/ray/experimental/ddp/src/main_element.py
+++ python/ray/experimental/ddp/src/main_element.py
@@ -8,17 +8,17 @@ from .core.config import parse_args
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.INFO,
     format="[%(levelname)s %(filename)s:%(lineno)d %(funcName)s] %(message)s",
 )
 
 
 class ElementModel(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_layers: int) -> None:
         super().__init__()
         self.device = "cuda:0"
         self.size = 1024
-        self.num_layers = 8
+        self.num_layers = num_layers
         self.linear_layers = torch.nn.ModuleList(
             [
                 torch.nn.Linear(
@@ -42,6 +42,7 @@ class ElementModel(torch.nn.Module):
         self.criterion = torch.nn.MSELoss()
         self.optimizer = torch.optim.SGD(self.linear_layers.parameters(), lr=0.01)
 
+    def init_weights(self) -> None:
         with torch.no_grad():
             for layer in self.linear_layers:
                 torch.nn.init.kaiming_uniform_(
@@ -62,7 +63,7 @@ class ElementModel(torch.nn.Module):
 
         return z
 
-    def backward_and_update_all_layers(self, pred: torch.Tensor):
+    def backward_aio(self, pred: torch.Tensor) -> None:
         self.optimizer.zero_grad()
         loss = self.criterion(pred, self.y)
         loss.backward()
@@ -137,8 +138,8 @@ def train_sequential(model: ElementModel, num_epochs: int, model_file: str) -> N
         pred = model.forward(model.x)
         logger.info(f"prediction: {pred}")
 
-        # Backward pass and update
-        model.backward_and_update_all_layers(pred)
+        # Backward and update all in one
+        model.backward_aio(pred)
 
         end = time.perf_counter()
 
@@ -152,6 +153,53 @@ def train_sequential(model: ElementModel, num_epochs: int, model_file: str) -> N
     with open(model_file, "w") as f:
         for layer in model.linear_layers:
             f.write(f"{layer.weight}\n")
+
+
+def train_cot(models: List[ElementModel], num_epochs: int, model_file: str) -> None:
+    assert len(models) == 2, "Only support two models for now"
+
+    for epoch in range(num_epochs):
+        start = time.perf_counter()
+
+        models[0].x = torch.randn(1, models[0].size, requires_grad=True).to(
+            models[0].device
+        )
+        models[1].y = torch.randn(1, models[1].size).to(models[1].device)
+
+        logger.info(f"epoch: {epoch}")
+        logger.info(f"input: {models[0].x}")
+        logger.info(f"target: {models[1].y}")
+
+        # Forward pass
+        pred1 = models[0].forward(models[0].x)
+        pred1_detached = pred1.detach().requires_grad_(True)
+        pred2 = models[1].forward(pred1_detached)
+        logger.info(f"prediction: {pred2}")
+
+        # Backward and update for all models
+        loss = models[1].criterion(pred2, models[1].y)
+        loss.backward()
+        models[1].optimizer.step()
+        models[1].optimizer.zero_grad()
+
+        pred1.backward(pred1_detached.grad)
+        models[0].optimizer.step()
+        models[0].optimizer.zero_grad()
+
+        end = time.perf_counter()
+
+        logger.info("updated weights:")
+        for model in models:
+            for idx, layer in enumerate(model.linear_layers):
+                logger.info(f"layer {idx} weight: {layer.weight}")
+
+        if epoch > 0:
+            logger.warning(f"epoch: {epoch} elapse: {round((end - start) * 1e6)} us")
+
+    with open(model_file, "w") as f:
+        for model in models:
+            for layer in model.linear_layers:
+                f.write(f"{layer.weight}\n")
 
 
 def train_checkpoint(model: ElementModel, num_epochs: int, model_file: str) -> None:
@@ -208,18 +256,46 @@ def train_checkpoint(model: ElementModel, num_epochs: int, model_file: str) -> N
 
 
 def main(args: Dict[str, Any]) -> None:
-    torch.manual_seed(998244353)
-
     logger.info("Welcome to Downton Abbey!")
 
-    model = ElementModel()
-    model = model.to(model.device)
-    num_epochs = 4
+    num_layers = 32
+    num_epochs = 10
 
     if args["mode"] == "sequential":
-        train_sequential(model, num_epochs, args["model_file"])
+        model = ElementModel(
+            num_layers=num_layers,
+        )
+
+        torch.manual_seed(998244353)
+        model.init_weights()
+        model = model.to(model.device)
+
+        train_sequential(
+            model,
+            num_epochs,
+            args["model_file"],
+        )
     elif args["mode"] == "checkpoint":
-        train_checkpoint(model, num_epochs, args["model_file"])
+        raise NotImplementedError
+    elif args["mode"] == "cot":
+        num_models = 2
+        models = [
+            ElementModel(
+                num_layers=num_layers // num_models,
+            )
+            for _ in range(num_models)
+        ]
+
+        torch.manual_seed(998244353)
+        for model in models:
+            model.init_weights()
+            model = model.to(model.device)
+
+        train_cot(
+            models,
+            num_epochs,
+            args["model_file"],
+        )
 
 
 if __name__ == "__main__":
