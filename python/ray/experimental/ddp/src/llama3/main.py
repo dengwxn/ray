08af75f38bcf866563d15165a4518367e37de1c6
Weixin Deng <weixin@cs.washington.edu>
2025-02-13 23:22:00 -0800

    chore: Main
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/src/llama3/main.py
+++ python/ray/experimental/ddp/src/llama3/main.py
@@ -1,5 +1,36 @@
+import os
+
+import torch
+from fairscale.nn.model_parallel.initialize import (
+    get_model_parallel_rank,
+    initialize_model_parallel,
+    model_parallel_is_initialized,
+)
+
+from .generation import Transformer
+from .model import LLAMA_1B
+
+
 def main():
-    print("Hello, world!")
+    os.environ["RANK"] = "0"
+    os.environ["WORLD_SIZE"] = "1"
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "25670"
+
+    if not torch.distributed.is_initialized():
+        torch.distributed.init_process_group("nccl")
+
+    if not model_parallel_is_initialized():
+        model_parallel_size = 1
+        initialize_model_parallel(model_parallel_size)
+
+    local_rank = 0
+    torch.cuda.set_device(local_rank)
+    torch.manual_seed(998244353)
+
+    model_args = LLAMA_1B
+    model = Transformer(model_args)
+    print(model)
 
 
 if __name__ == "__main__":
