c5d6f5ad97e52e8200e84790f9e933aa7e96b8fb
Weixin Deng <weixin@cs.washington.edu>
2025-02-16 13:06:01 -0800

    refactor: folders
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/src/main/llama3/example_e2e.py
+++ python/ray/experimental/ddp/src/main/llama3/example_e2e.py
@@ -7,8 +7,8 @@ from fairscale.nn.model_parallel.initialize import (
     model_parallel_is_initialized,
 )
 
-from .generation import Transformer
-from .model import LLAMA_1B, LLAMA_3B, LLAMA_8B
+from ...core.llama3.generation import Transformer
+from ...core.llama3.model import LLAMA_1B, LLAMA_3B, LLAMA_8B
 
 
 def main():
