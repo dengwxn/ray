83c68793562c201739bc87d7498b4f5a2c83f1b7
Weixin Deng <weixin@cs.washington.edu>
2025-02-16 14:47:53 -0800

    chore: fix
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/src/main/llama3/single/training.py
+++ python/ray/experimental/ddp/src/main/llama3/single/training.py
@@ -8,8 +8,8 @@ from fairscale.nn.model_parallel.initialize import (
     model_parallel_is_initialized,
 )
 
-from ...core.llama3.generation import Transformer
-from ...core.llama3.model import LLAMA_1B, LLAMA_3B, LLAMA_8B
+from ....core.llama3.generation import Transformer
+from ....core.llama3.model import LLAMA_1B, LLAMA_3B, LLAMA_8B
 
 
 def main():
