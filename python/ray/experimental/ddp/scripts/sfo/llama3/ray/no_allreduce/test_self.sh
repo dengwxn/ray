5ef4692b7a7fa84c39fe49b405fa0cff61c2be8c
Weixin Deng <weixin@cs.washington.edu>
2025-02-19 21:17:49 +0000

    chore: scripts
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/ddp/scripts/sfo/llama3/ray/no_allreduce/test_self.sh
+++ python/ray/experimental/ddp/scripts/sfo/llama3/ray/no_allreduce/test_self.sh
@@ -31,7 +31,7 @@ mkdir -p $output_path
 
 num_partitions=18
 num_actors=2
-num_iters=3
+num_iters=20
 latency_prefix=${timestamp}
 model_prefix=$output_path/${timestamp}_model
 log_file=$output_path/${timestamp}.log
