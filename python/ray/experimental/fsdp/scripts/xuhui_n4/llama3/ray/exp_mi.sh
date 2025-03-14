234d9d18508bc8326d8410215291fe03c9895d95
Weixin Deng <weixin@cs.washington.edu>
2025-03-13 18:36:12 -0700

    chore: scripts
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

---
+++ python/ray/experimental/fsdp/scripts/xuhui_n4/llama3/ray/exp_mi.sh
@@ -0,0 +1,9 @@
+# cc_off
+# scripts/xuhui_n4/llama3/ray/cc_off/origin/exp_self.sh
+scripts/xuhui_n4/llama3/ray/cc_off/ov_off/exp_self.sh
+# scripts/xuhui_n4/llama3/ray/cc_off/ov_on/exp_self.sh
+
+# cc_on
+# scripts/xuhui_n4/llama3/ray/cc_on/origin/exp_self.sh
+scripts/xuhui_n4/llama3/ray/cc_on/ov_off/exp_self.sh
+scripts/xuhui_n4/llama3/ray/cc_on/ov_on/exp_self.sh
