425760b7400e04a6c4fb80931200339da9323ecd
Weixin Deng <weixin@cs.washington.edu>
2025-03-18 02:25:49 +0000

    chore: clean
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/experimental/1f1b/scripts/barbell/llama3/ray/exp_mi.sh
+++
@@ -1,9 +0,0 @@
-# p2p_off
-# scripts/barbell/llama3/ray/p2p_off/origin/exp_self.sh
-scripts/barbell/llama3/ray/p2p_off/ov_off/exp_self.sh
-# scripts/barbell/llama3/ray/p2p_off/ov_on/exp_self.sh
-
-# p2p_on
-# scripts/barbell/llama3/ray/p2p_on/origin/exp_self.sh
-scripts/barbell/llama3/ray/p2p_on/ov_off/exp_self.sh
-# scripts/barbell/llama3/ray/p2p_on/ov_on/exp_self.sh
--- python/ray/experimental/1f1b/scripts/xuhui_n2/llama3/ray/exp_all.sh
+++
@@ -1,9 +0,0 @@
-# p2p_off
-scripts/xuhui_n2/llama3/ray/p2p_off/origin/exp_self.sh
-scripts/xuhui_n2/llama3/ray/p2p_off/ov_off/exp_self.sh
-scripts/xuhui_n2/llama3/ray/p2p_off/ov_on/exp_self.sh
-
-# p2p_on
-scripts/xuhui_n2/llama3/ray/p2p_on/origin/exp_self.sh
-scripts/xuhui_n2/llama3/ray/p2p_on/ov_off/exp_self.sh
-scripts/xuhui_n2/llama3/ray/p2p_on/ov_on/exp_self.sh
